cc34cc8b6461e277b6d3e3b701d3d8720ae91612
jameswsullivan <81947235+jameswsullivan@users.noreply.github.com>
2024-01-18 14:51:12 -0600

    Update featured_content

---
+++ onisite/plugins/featured_content/config.py
@@ -0,0 +1,33 @@
+# Set to True to use random pages instead of curated selections in PAGES
+RANDOM = True
+
+# Set to True to enable the 'This day in history' function
+THISDAY = False
+
+# Set to earliest year to search with THISDAY
+MINYEAR = 1500
+
+# Set to latest year to search  with THISDAY
+MAXYEAR = 2024
+
+# The number of results that will be displayed
+NUMBER = 4
+
+# Specific featured pages to display
+
+# PAGES = (
+#           {
+#               'lccn': 'sn83045350',
+#               'date': '1878-01-03',
+#               'edition': 1,
+#               'sequence': 1,
+#               'caption': 'Put a caption for your newspaper here'
+#           },
+#           {
+#               'lccn': 'sn83045350',
+#               'date': '1878-01-03',
+#               'edition': 1,
+#               'sequence': 1,
+#               'caption': 'This is the second one'
+#           },
+#         )
